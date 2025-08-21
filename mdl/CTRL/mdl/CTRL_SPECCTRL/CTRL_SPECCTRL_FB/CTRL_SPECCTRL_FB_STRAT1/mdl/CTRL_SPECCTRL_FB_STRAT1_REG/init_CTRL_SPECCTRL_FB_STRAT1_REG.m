8d58efb602860a7405a9f67a46ae1404ec7057b3
ondrra <blatak@fme.vutbr.cz>
2025-08-21 11:31:17 +0200

    Feedback block finished in rough

---
+++ mdl/CTRL/mdl/CTRL_SPECCTRL/CTRL_SPECCTRL_FB/CTRL_SPECCTRL_FB_STRAT1/mdl/CTRL_SPECCTRL_FB_STRAT1_REG/init_CTRL_SPECCTRL_FB_STRAT1_REG.m
@@ -0,0 +1,5 @@
+%init
+
+cal_def('CTRL_SPECCTRL_FB_STRAT1_REG_DmpUpLim_p100',100,0,1000,'single',1,'p100','---')
+cal_def('CTRL_SPECCTRL_FB_STRAT1_REG_DmpLoLim_p100',0,0,1000,'single',1,'p100','---')
+
---
+++ mdl/CTRL/mdl/CTRL_SPECCTRL/CTRL_SPECCTRL_FB/init_CTRL_SPECCTRL_FB.m
@@ -0,0 +1,3 @@
+%init
+
+cal_def('CTRL_SPECCTRL_FB_p_SwtFbStrat_na',1,0,1000,'single',1,'na','---')
